d66d33aba5ee7f89133335eafcc4fd1d23458b1b
hzg <hzg@qq.com>
2016-08-28 09:29:32 +0800

    积少成多

---
+++ javascript/jquery/class.php
@@ -0,0 +1,19 @@
+//---判断是否存在相应的class---\
+返回 true false
+if ($(this).hasClass('click')) {
+}
+//---判断是否存在相应的class---/
+
+//移除
+Obj.removeClass("click");
+
+//增加
+Obj.addClass("dog");
+
+
+
+
+
+
+
+
